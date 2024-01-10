92973e2f165fcf2a971113aa204019df2e72d02c
Anuskota <anamangada.dev@gmail.com>
2024-01-11 00:37:21 +0100

    eventos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import atanagildo from "./img/rey_atanagildo.png";
+import sisebuto from "./img/rey_sisebuto.png"
+import "./App.css";
+import React, { useRef } from "react";
 
 function App() {
+  const refBox = useRef();
+  const change = 23.16;
+
+  const increment = (e) => {
+   
+    e.target.innerHTML = Number(e.target.innerHTML) + 1;
+    if (e.target.innerHTML >= 10) {
+      e.target.innerHTML = 1;
+    }
+    if (e.target.innerHTML >= 8) {
+      e.target.style.backgroundColor = "red";
+    } else {
+       e.target.style.backgroundColor = "white";
+    }
+  } 
+    
+  const changeCoin = ()=> {
+    refBox.current.innerHTML = Number(refBox.current.innerHTML) * change
+  };
+  const changeImg = (j) => {
+    if (j.target.src.includes("atanagildo")) {
+        j.target.src=sisebuto
+    } else {
+      j.target.src = atanagildo;
+      }
+      
+  }
+  const read = (e) => {
+    refBox.current.innerHTML = e.target.value;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div ref={refBox} className="box" onClick={increment}>
+        1
+      </div>
+      <button onClick={changeCoin}>Aceptar</button>
+      <div>
+        <img onClick={changeImg} src={atanagildo} alt="imagen" />
+      </div>
+      <input onChange={read } className="camp" />
+    </>
   );
 }
 
